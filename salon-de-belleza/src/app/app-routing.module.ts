5c6258081667773b690d14bfbfba579a7b1691ce
Javier Alberto Alfaro Jara <javier.alfaro.jara@gmail.com>
2021-11-25 16:46:01 -0600

    ruteado y modulos material

--- salon-de-belleza/src/app/app-routing.module.ts
+++ salon-de-belleza/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VistaListaUsuariosComponent } from './components/vista-lista-usuarios/vista-lista-usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'vista-usuarios', component: VistaListaUsuariosComponent},
+  {path: '**', redirectTo: 'vista-usuarios'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- salon-de-belleza/src/app/app.module.ts
+++ salon-de-belleza/src/app/app.module.ts
@@ -7,6 +7,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { VistaListaUsuariosComponent } from './components/vista-lista-usuarios/vista-lista-usuarios.component';
 import { DialogoVerCitasUsuarioComponent } from './components/dialogs/dialogo-ver-citas-usuario/dialogo-ver-citas-usuario.component';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +31,23 @@ import { DialogoVerCitasUsuarioComponent } from './components/dialogs/dialogo-ve
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatListModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatDialogModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
